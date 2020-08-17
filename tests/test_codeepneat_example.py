ff50ca378fa81989a52df737b24c15b8eeadcc6f
Paul Pauls <mail@paulpauls.de>
2020-08-17 18:13:15 +0200

    Add basic cdn test example

---
+++ tests/test_codeepneat_example.py
@@ -0,0 +1,26 @@
+import tempfile
+
+import tfne
+
+
+def test_codeepneat_example():
+    """"""
+    # Initialize the environment and the specific NE algorithm
+    config = tfne.parse_configuration('./test_codeepneat_example_config.cfg')
+    environment = tfne.environments.XOREnvironment(config)
+    ne_algorithm = tfne.algorithms.CoDeepNEAT(config, environment)
+
+    # Initialize evolution engine and supply config as well as initialized NE elements. As CoDeepNEAT currently only
+    # supports single-instance evaluation, set num_cpus and num_gpus to 1
+    engine = tfne.EvolutionEngine(ne_algorithm=ne_algorithm,
+                                  backup_dir_path=tempfile.gettempdir(),
+                                  num_cpus=1,
+                                  num_gpus=1,
+                                  max_generations=10,
+                                  max_fitness=None)
+
+    # Start training process, returning the best genome when training ends
+    best_genome = engine.train()
+
+    best_genome_fitness = environment.eval_genome_fitness(best_genome)
+    assert 1.0 >= best_genome_fitness > 0
