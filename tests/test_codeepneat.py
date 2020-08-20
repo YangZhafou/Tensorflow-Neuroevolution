4a2bb1bff71c02cb7ed09126a12df760f1a2cd0c
Paul Pauls <mail@paulpauls.de>
2020-08-20 18:23:59 +0200

    Add CIFAR10 env as fifth test example

--- tests/test_codeepneat.py
+++ tests/test_codeepneat.py
@@ -88,3 +88,23 @@ def test_codeepneat_4():
 
     # Sanity check state of the algorithm
     sanity_check_algorithm_state(ne_algorithm)
+
+
+def test_codeepneat_5():
+    """"""
+    # Create test config
+    config = tfne.parse_configuration(os.path.dirname(__file__) + '/test_codeepneat_4_config.cfg')
+    environment = tfne.environments.CIFAR10Environment(config)
+    ne_algorithm = tfne.algorithms.CoDeepNEAT(config, environment)
+
+    # Start test
+    engine = tfne.EvolutionEngine(ne_algorithm=ne_algorithm,
+                                  backup_dir_path=tempfile.gettempdir(),
+                                  num_cpus=1,
+                                  num_gpus=1,
+                                  max_generations=2,
+                                  max_fitness=None)
+    engine.train()
+
+    # Sanity check state of the algorithm
+    sanity_check_algorithm_state(ne_algorithm)
